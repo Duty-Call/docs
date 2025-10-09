4c2498feccca54f5b5dd3164bafc420abaa72918
chrisberno <chris.berno@gmail.com>
2025-10-09 09:39:19 -0400

    Configure custom domain docs.dutycall.net
    - Updated baseUrl from /docs/ to / (custom domains use root)
    - Updated url from duty-call.github.io to docs.dutycall.net
    - Added CNAME to workflow to persist custom domain on deployment
    
    DNS configured and verified. HTTPS certificate approved.
    Site now accessible at https://docs.dutycall.net

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://duty-call.github.io',
+  url: 'https://docs.dutycall.net',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'dutycall',
