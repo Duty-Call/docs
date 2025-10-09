9c9ee189ccd4c808d934eccdb81751e07a435488
chrisberno <chris.berno@gmail.com>
2025-10-09 09:09:08 -0400

    Fix baseUrl for GitHub Pages deployment
    GitHub Pages org repos deploy to org.github.io/repo-name/
    Changed baseUrl from / to /docs/ to match deployment path.
    
    When custom domain is configured, this will be updated to:
    - url: https://docs.dutycall.net
    - baseUrl: /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.dutycall.net',
+  url: 'https://duty-call.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   organizationName: 'dutycall',
